343d5ffced82b8445b961140c192d8486bbd6018
julitree <julitreedesign@gmail.com>
2021-04-28 13:26:06 +0200

    learnig get info APIs

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-let x = {}
-
 fetch('https://breakingbadapi.com/api/characters')
     .then(response => {
         return response.json();
@@ -10,7 +8,9 @@ fetch('https://breakingbadapi.com/api/characters')
             let cardContainer = document.createElement("div");
             cardContainer.classList.add("cardContainer");
             cardContainer.innerHTML = `
-                <div class="imgContainer"><img src=${character.img}></div>
+                <div class="imgContainer">
+                    <img src=${character.img}>
+                </div>
                 <div class="infoContainer">
                     <h2>${character.name}</h2>
                     <p>Nickname: <span>${character.nickname}</span></p>
@@ -19,18 +19,6 @@ fetch('https://breakingbadapi.com/api/characters')
                 </div>`;
 
             gridContainer.appendChild(cardContainer)
-                // let cardContainer = document.createElement("div");
-
-            // let imgContainer = document.createElement("div");
-            // let imgCharacter = document.createElement("IMG");
-
-            // let InfoContainer = document.createElement("div");
-            // let nameCharacter = document.createElement("h2");
-            // let nickname = document.createElement("p");
-            // let occupation = document.createElement("p");
-            // let breakingBadSeasons = document.createElement("p");
-            // let status = document.createElement("p");
-
 
             console.log(character.name);
         });
